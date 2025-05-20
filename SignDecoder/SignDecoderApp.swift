014540f753f9485e6c0a573f57490ba62e80d22d
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2025-05-20 23:42:03 +0900

    Initial Commit

---
+++ SignDecoder/SignDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignDecoderApp.swift
+//  SignDecoder
+//
+//  Created by Oliver Park on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SignDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SignDecoderTests/SignDecoderTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignDecoderTests.swift
+//  SignDecoderTests
+//
+//  Created by Oliver Park on 5/20/25.
+//
+
+import Testing
+@testable import SignDecoder
+
+struct SignDecoderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
