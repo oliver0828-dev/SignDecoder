05ece835814fdd99a7d05528e1da3bef7909f7e5
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2025-05-23 15:09:49 +0900

    May23

---
+++ SignDecoder/TextRecognitionView.swift
@@ -0,0 +1,18 @@
+//
+//  TextRecognitionView.swift
+//  SignDecoder
+//
+//  Created by Oliver Park on 5/23/25.
+//
+
+import SwiftUI
+
+struct TextRecognitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextRecognitionView()
+}
---
+++ SignDecoder/TextRecognizer.swift
@@ -0,0 +1,8 @@
+//
+//  TextRecognizer.swift
+//  SignDecoder
+//
+//  Created by Oliver Park on 5/23/25.
+//
+
+import Foundation
